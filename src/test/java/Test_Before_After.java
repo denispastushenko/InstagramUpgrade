ba0700feed2c72a4df93fbcf1c2e2b71d42b09c9
Padavan1 <dpastushenko1996@mail.com>
2016-11-17 15:30:07 +0200

    SuccCommitCh

--- src/test/java/Test_Before_After.java
+++ src/test/java/Test_Before_After.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import pages.AssertPage;
 import pages.Page;
-import pages.TestPage;
 
 import java.awt.*;
 import java.net.MalformedURLException;
@@ -17,13 +16,13 @@ public class Test_Before_After {
     protected PageObject photoPage;*/
     public static final String site = "https://www.epam.com";
    // protected PageObject pageObject;
-    protected TestPage testPage;
+    //protected TestPage testPage;
     protected AssertPage assertPage;
 
     @Before
     public void setUp() throws MalformedURLException, AWTException {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Denys\\Desktop\\chromedriver_win32\\chromedriver.exe");
-        testPage = new TestPage(new RemoteWebDriver(new URL("http://localhost:4445/wd/hub"), DesiredCapabilities.firefox()));
+        assertPage = new AssertPage(new RemoteWebDriver(new URL("http://localhost:4445/wd/hub"), DesiredCapabilities.firefox()));
        // photoPage = new PageObject(new ChromeDriver());
         Page.getDriver().manage().window().maximize();
     }
